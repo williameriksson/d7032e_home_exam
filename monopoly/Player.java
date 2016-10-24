76fe538425a56dc33b2583f6b3184172fcfd27a2
William Eriksson <willeeh@hotmail.com>
2016-10-24 19:09:46 +0200

    Just minor change in player.java

--- monopoly/Player.java
+++ monopoly/Player.java
@@ -1,6 +1,6 @@
 package monopoly;
 import java.util.ArrayList;
-import monopoly.*;
+
 
 
 public class Player {
@@ -9,7 +9,6 @@ public class Player {
   public int money = 200;
   public int knowledge = 0;
   public boolean computer = false;
-  public ArrayList<Integer> ownsTile = new ArrayList<Integer>();
   public boolean stillPlaying = true;
   public boolean skipOneTurn = false;
 
@@ -24,10 +23,7 @@ public class Player {
   }
 
   public void setStillPlaying(boolean playing) {
-    if(!false) {
-      stillPlaying = playing;
-      name = "         ";
-      ownsTile = new ArrayList<Integer>();
-    }
+    stillPlaying = playing;
   }
+  
 }
